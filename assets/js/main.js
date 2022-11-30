7b7abee491068a6369f53cdd7b588425fd0a864e
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-11-30 22:17:50 +0600

    note save method created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 import NotesAPI from '../js/NotesAPI.js';
 
+// NotesAPI.saveNote(
+//     {
+//         title: 'New Note!',
+//         body: 'I am a new note'
+//     }
+// )
 
 console.log(NotesAPI.getAllNotes());
